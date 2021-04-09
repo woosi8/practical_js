585dde4fc198cb2c14716fd21380ee236693fda0
DESKTOP-QNTD4OJ\Asher <tmfvmehek@gmail.com>
2021-04-09 22:24:09 +0900

    change class_name

--- click_event/main.js
+++ click_event/main.js
@@ -6,16 +6,16 @@
 // 	$(".ssss").addClass("selected");
 // });
 
-const workBtnContainer = document.querySelector("#btn");
-const projectContainer = document.querySelector("#the-box");
-const proejects = document.querySelectorAll(".ssss");
-workBtnContainer.addEventListener("click", (event) => {
+const btn = document.querySelector("#btn");
+const objects = document.querySelectorAll(".ssss");
+// const projectContainer = document.querySelector("#the-box");
+btn.addEventListener("click", (event) => {
 	const filter =
 		event.target.dataset.filter || event.target.parentNode.dataset.filter;
 	if (filter == null) {
 		return;
 	}
-	proejects.forEach((project) => {
+	objects.forEach((project) => {
 		if (filter === project.dataset.type) {
 			project.classList.remove("invisible");
 		} else {
